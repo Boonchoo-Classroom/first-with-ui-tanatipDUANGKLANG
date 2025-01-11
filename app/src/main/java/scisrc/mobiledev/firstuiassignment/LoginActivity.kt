96aee73e64f881b81183684cbdc588b645c080b3
oil <tanatip.d@ku.th>
2025-01-12 01:42:47 +0700

    testfirstcommit

--- app/src/main/java/scisrc/mobiledev/firstuiassignment/LoginActivity.kt
+++ app/src/main/java/scisrc/mobiledev/firstuiassignment/LoginActivity.kt
@@ -2,10 +2,10 @@ package scisrc.mobiledev.firstuiassignment
 
 import android.os.Bundle
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 class LoginActivity : AppCompatActivity() {
 
@@ -13,8 +13,11 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_login)
+
+        // ทำให้หน้าจอแสดงเต็มพื้นที่ (Edge-to-Edge)
+        WindowInsetsControllerCompat(window, window.decorView).isAppearanceLightStatusBars = true
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -23,7 +26,7 @@ class LoginActivity : AppCompatActivity() {
 
         backToMainBtn = findViewById(R.id.loginBackToMainBtn)
 
-        backToMainBtn.setOnClickListener() {
+        backToMainBtn.setOnClickListener {
             finish()
         }
     }
