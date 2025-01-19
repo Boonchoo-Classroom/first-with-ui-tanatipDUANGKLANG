5d747ec0b321ce057ab539605ed04dcfe8f8811a
oil <tanatip.d@ku.th>
2025-01-19 22:01:16 +0700

    edit register by oil

--- app/src/main/java/scisrc/mobiledev/firstuiassignment/RegisterActivity.kt
+++ app/src/main/java/scisrc/mobiledev/firstuiassignment/RegisterActivity.kt
@@ -2,10 +2,10 @@ package scisrc.mobiledev.firstuiassignment
 
 import android.os.Bundle
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 class RegisterActivity : AppCompatActivity() {
 
@@ -13,8 +13,11 @@ class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_register)
+
+        // ทำให้หน้าจอแสดงเต็มพื้นที่ (Edge-to-Edge)
+        WindowInsetsControllerCompat(window, window.decorView).isAppearanceLightStatusBars = true
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -23,7 +26,7 @@ class RegisterActivity : AppCompatActivity() {
 
         backToMainPageBtn = findViewById(R.id.registBackToMainBtn)
 
-        backToMainPageBtn.setOnClickListener() {
+        backToMainPageBtn.setOnClickListener {
             finish()
         }
     }
